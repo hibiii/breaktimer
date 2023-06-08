ea15fcb730522398043796a6fd97f743096a932d
hibi <69405847+hibiii@users.noreply.github.com>
2023-06-08 13:13:17 -0300

    Add basic signalling

--- build.zig
+++ build.zig
@@ -11,6 +11,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    main_exe.linkLibC();
 
     b.installArtifact(main_exe);
 
@@ -25,14 +26,14 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run-main", "Run brktimer");
     run_step.dependOn(&run_main.step);
 
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+    // const unit_tests = b.addTest(.{
+    //     .root_source_file = .{ .path = "src/main.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
-    const run_unit_tests = b.addRunArtifact(unit_tests);
+    // const run_unit_tests = b.addRunArtifact(unit_tests);
 
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    // const test_step = b.step("test", "Run unit tests");
+    // test_step.dependOn(&run_unit_tests.step);
 }
---
+++ src/signals.zig
@@ -0,0 +1,30 @@
+const signal = @cImport({
+    @cInclude("signal.h");
+});
+
+/// Alarm signal sent to the process when it wakes up from sleep.
+/// Should not be sent manually under normal circumstances.
+pub const Alarm = signal.SIGALRM;
+
+/// Sent when the user acknowledges the alarm.
+pub const Acknowledge = signal.SIGUSR1;
+
+/// Sent when the user leaves the computer.
+pub const Standup = signal.SIGUSR2;
+
+/// Sent when the user sits down at the computer.
+///
+/// SIGCONT has the positive side effect of continuing program execution, this
+/// can be coupled with a self-raised SIGSTOP after a standup signal to reduce
+/// CPU usage.
+pub const Sitdown = signal.SIGCONT;
+
+pub const Error = error{
+    HandlersDisallowedForSignal,
+};
+
+pub fn register(signal_number: c_int, comptime handler: fn (c_int) callconv(.C) void) !void {
+    if (signal.signal(signal_number, handler) == signal.SIG_ERR) {
+        return Error.HandlersDisallowedForSignal;
+    }
+}
